c058509608f0f23a90111e5bcab84c6b61d81c1d
meky-vodafone <mahmoud.meky.vodafone@gmail.com>
2025-10-08 13:00:00 +0300

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { OnirixService } from './onirix.service';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports:[FormsModule]
 })
 export class AppComponent {
   private onirixService = inject(OnirixService);
+  link: string = ''; // input value bound to input field
 
   ngOnInit() {
     this.onirixService.init();
