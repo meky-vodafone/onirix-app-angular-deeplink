eca683de5c40edff1f119168b7cdf8b1e24895cc
meky-vodafone <mahmoud.meky.vodafone@gmail.com>
2025-11-05 16:08:26 +0200

    alert

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   imports: [RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  caughtCharacter: boolean | null = null;
+  time: string | null = null;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    const params = this.route.snapshot.queryParams;
+
+    // ✅ Check if there are any query params
+    if (Object.keys(params).length > 0) {
+      console.log('Raw query params:', params);
+
+      // Convert from string to proper types
+      this.caughtCharacter =
+        params['caughtCharachter'] === 'true'
+          ? true
+          : params['caughtCharachter'] === 'false'
+          ? false
+          : null;
+
+      this.time = params['time'] === 'null' ? null : params['time'];
+
+      console.log('caughtCharacter:', this.caughtCharacter);
+      console.log('time:', this.time);
+
+      // ✅ Only alert if params exist
+      alert(`caught: ${this.caughtCharacter} & time=${this.time}`);
+    }
+  }
 }
